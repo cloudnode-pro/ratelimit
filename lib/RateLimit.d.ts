3574991d197e3e10f2f0f8c58ef515dda9ceb43f
williamd5 <willduncannn@gmail.com>
2022-10-08 14:43:30 +0300

    build

--- lib/RateLimit.d.ts
+++ lib/RateLimit.d.ts
@@ -1,10 +1,18 @@
 import { AttemptResult } from "./AttemptResult";
+import { RateLimitSettings } from "./RateLimitSettings";
+import e from "express";
 /**
  * Rate limit
  * @class
  */
 export declare class RateLimit {
     #private;
+    /**
+     * Global rate limit settings. These will apply to all rate limits.
+     * @type {RateLimitSettings}
+     * @static
+     */
+    static settings: RateLimitSettings;
     /**
      * Name of the rate limit
      * @readonly
@@ -22,14 +30,20 @@ export declare class RateLimit {
      */
     timeWindow: number;
     /**
+     * Settings for this rate limit
+     * @type {RateLimitSettings}
+     */
+    settings: RateLimitSettings;
+    /**
      * Create a new rate limit
      * @param {string} name - The name of the rate limit
      * @param {number} limit - The number of requests allowed per time window (e.g. 60)
      * @param {number} timeWindow - The time window in seconds (e.g. 60)
+     * @param {RateLimitSettings|Record<string, any>} [settings] - Settings for this rate limit
      * @returns {RateLimit}
      * @throws {Error} - If the rate limit already exists
      */
-    constructor(name: string, limit: number, timeWindow: number);
+    constructor(name: string, limit: number, timeWindow: number, settings?: RateLimitSettings | Record<string, any>);
     /**
      * Check the attempt state for a source ID without decrementing the remaining attempts
      * @param {string} source - Unique source identifier (e.g. username, IP, etc.)
@@ -51,6 +65,29 @@ export declare class RateLimit {
      * @returns {void}
      */
     reset(source: string): void;
+    /**
+     * Make a rate limit attempt and also send rate limit headers.
+     * @param {string} source - Unique source identifier (e.g. username, IP, etc.)
+     * @param {e.Request} req - An Express request object
+     * @param {e.Response} res - An Express response object
+     * @returns {AttemptResult}
+     */
+    request(source: string, req: e.Request, res: e.Response): AttemptResult;
+    /**
+     * Send rate limit response that is set in the settings.
+     * @param {AttemptResult} attempt - The attempt result
+     * @param {e.Request} req - An Express request object
+     * @param {e.Response} res - An Express response object
+     * @param {e.NextFunction} [next] - Call next middleware
+     * @returns {void}
+     */
+    response(attempt: AttemptResult, req: e.Request, res: e.Response, next?: e.NextFunction): void;
+    /**
+     * Express.js middleware to make a rate limit attempt and also send rate limit headers.
+     * @param {function(e.Request): string} source - A function that is called with the Express request object and returns a unique source identifier (e.g. username, IP, etc.)
+     * @returns {e.RequestHandler}
+     */
+    middleware(source: (req: e.Request) => string): e.RequestHandler;
     /**
      * Set the remaining attempts for a source ID.
      * > **Warning**: This is not recommended as the remaining attempts depend on the limit of the instance.
@@ -106,6 +143,41 @@ export declare class RateLimit {
      * @static
      */
     static reset(name: string, source: string): void;
+    /**
+     * Make a rate limit attempt and also send rate limit headers.
+     * @param {string} name - The name of the rate limit
+     * @param {string} source - Unique source identifier (e.g. username, IP, etc.)
+     * @param {e.Request} req - An Express request object
+     * @param {e.Response} res - An Express response object
+     * @returns {AttemptResult}
+     * @throws {Error} - If the rate limit does not exist
+     * @static
+     */
+    static request(name: string, source: string, req: e.Request, res: e.Response): AttemptResult;
+    /**
+     * Send rate limit response that is set in the settings.
+     * @param {string} [name=null] - The name of the rate limit. Set to `null` to use global settings. Rate limit instances
+     * that have not explicitly set a response will automatically inherit the global response setting at the time of
+     * construction.
+     * @param {AttemptResult} attempt - The attempt result
+     * @param {e.Request} req - An Express request object
+     * @param {e.Response} res - An Express response object
+     * @param {e.NextFunction} [next] - An Express next function
+     * @returns {void}
+     * @throws {Error} - If the rate limit does not exist (never thrown if `name` is `null`)
+     * @static
+     */
+    static response(name: string | undefined, attempt: AttemptResult, req: e.Request, res: e.Response, next?: e.NextFunction): void;
+    /**
+     * Express.js middleware to make a rate limit attempt and also send rate limit headers.
+     * @param {string} name - The name of the rate limit
+     * @param {function(e.Request): string} source - A function that is called with the Express request object and
+     * returns a unique source identifier (e.g. username, IP, etc.)
+     * @returns {e.RequestHandler}
+     * @throws {Error} - If the rate limit does not exist
+     * @static
+     */
+    static middleware(name: string, source: (req: e.Request) => string): e.RequestHandler;
     /**
      * Set the remaining attempts for a source ID.
      * > **Warning**: This is not recommended as the remaining attempts depend on the limit of the instance.
---
+++ lib/RateLimitSettings.d.ts
@@ -0,0 +1,61 @@
+import { AttemptResult } from "./AttemptResult";
+import e from "express";
+/**
+ * Rate limit settings
+ * @interface RateLimitSettings
+ */
+export interface RateLimitSettings {
+    /**
+     * Automatically send rate limit headers when using `.request()` and `.middleware()`. Individual headers can be disabled by setting them to `null` in the `headers` object.
+     * @type {boolean}
+     * @default true
+     */
+    sendHeaders: boolean;
+    /**
+     * Name of the headers to send. Set a specific header to `null` to disable it. This has no effect if `sendHeaders` is `false`.
+     * @type {Record<string, string | null>}
+     */
+    headers: {
+        /**
+         * Indicates the service limit associated to the client in the current time-window.
+         * @type {string | null}
+         * @default 'RateLimit-Limit'
+         */
+        limit: string | null;
+        /**
+         * Indicates the remaining quota units (attempts).
+         * @type {string | null}
+         * @default 'RateLimit-Remaining'
+         */
+        remaining: string | null;
+        /**
+         * Indicates the number of seconds until the quota resets.
+         * @type {string | null}
+         */
+        reset: string | null;
+        /**
+         * Indicates the quota associated to the client and its value is informative. See [IETF RateLimit Fields for HTTP, Section 2.3](https://datatracker.ietf.org/doc/html/draft-ietf-httpapi-ratelimit-headers#section-2.3)
+         * @type {string | null}
+         * @experimental Policy reporting is not implemented yet. This is reserved for future use.
+         * @TODO Implement policy reporting
+         */
+        policy: string | null;
+    };
+    /**
+     * Function to transform the value of the `reset` header. This has no effect if `sendHeaders` is `false`.
+     * @param {number} reset - The remaining time in seconds until the quota resets
+     * @returns {string}
+     * @default (reset) => reset.toString()
+     */
+    resetHeaderValue: (reset: number) => string;
+    /**
+     * A function to send a "429 Too Many Requests" response. This is used when {@link RateLimit.middleware} is used or if {@link RateLimit.sendDefaultResponse} is manually invoked.
+     * @param {AttemptResult} attempt - The rate limit attempt result
+     * @param {e.Request} req - The express request object
+     * @param {e.Response} res - The express response object
+     * @param {e.NextFunction} next - The express next() function
+     * @returns {void}
+     */
+    defaultResponse: (attempt: AttemptResult, req: e.Request, res: e.Response, next?: e.NextFunction) => void;
+}
+//# sourceMappingURL=RateLimitSettings.d.ts.map
