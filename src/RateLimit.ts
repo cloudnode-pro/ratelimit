6ac13d1d821fc9124b30180589317929a51278bd
williamd5 <willduncannn@gmail.com>
2022-10-09 00:28:48 +0300

    add settings property to `.create()`

--- src/RateLimit.ts
+++ src/RateLimit.ts
@@ -396,12 +396,13 @@ export class RateLimit {
      * @param {string} name - The name of the rate limit
      * @param {number} limit - The number of attempts allowed per time window (e.g. 60)
      * @param {number} timeWindow - The time window in seconds (e.g. 60)
+     * @param {RateLimitSettings|Record<string, any>} [settings] - Settings for this rate limit
      * @returns {RateLimit}
      * @static
      */
-    static create(name: string, limit: number, timeWindow: number): RateLimit {
+    static create(name: string, limit: number, timeWindow: number, settings?: RateLimitSettings | Record<string, any>): RateLimit {
         const existing = RateLimit.get(name);
         if (existing) return existing;
-        return new RateLimit(name, limit, timeWindow);
+        return new RateLimit(name, limit, timeWindow, settings);
     }
 }
